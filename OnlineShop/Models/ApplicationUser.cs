b6c85f8e3e61cf251a349e75de8dbe8cc1dba68b
Радмир <radmirsalim04@gmail.com>
2025-06-08 16:53:45 +0300

    Реализация пользователей, красивая регистрация

---
+++ OnlineShop/Models/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? DisplayName { get; set; }  //имя(отображается в профиле)
+}
--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
 using OnlineShop.Services;
@@ -9,7 +9,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+
+    options.SignIn.RequireConfirmedAccount = false;
+
+    options.Password.RequireDigit = false;              // не требовать цифру
+    options.Password.RequireLowercase = false;          // не требовать строчные буквы
+    options.Password.RequireUppercase = false;          // не требовать заглавные буквы
+    options.Password.RequireNonAlphanumeric = false;    // не требовать спецсимволы
+    options.Password.RequiredLength = 4;                // минимальная длина пароля
+    options.Password.RequiredUniqueChars = 0;           // не требовать уникальные символы
+
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
@@ -42,5 +54,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate(); // применить миграции, если нужно
+    DbInitializer.Seed(context); // вызываем метод для заполнения товарами
+}
 
 app.Run();
