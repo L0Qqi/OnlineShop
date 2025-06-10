020f668be0f9db2bbfdcdefce8597abb107eafed
Радмир <radmirsalim04@gmail.com>
2025-06-10 13:13:31 +0300

    add products with images

--- OnlineShop/Data/DbInitializer.cs
+++ OnlineShop/Data/DbInitializer.cs
@@ -13,23 +13,47 @@ namespace OnlineShop.Data
                 new Product
                 {
                     Name = "Футболка",
-                    Description = "Классическая хлопковая футболка",
+                    Description = "Классическая хлопковая футболка Puma",
                     Price = 1299,
-                    ImageUrl = "https://via.placeholder.com/300x200"
+                    ImageUrl = "/images/t-shirt1.jpg"
                 },
                 new Product
                 {
                     Name = "Джинсы",
                     Description = "Синие джинсы slim fit",
                     Price = 2999,
-                    ImageUrl = "https://via.placeholder.com/300x200"
+                    ImageUrl = "/images/jeans1.jpg"
                 },
                 new Product
                 {
-                    Name = "Кроссовки",
-                    Description = "Удобные повседневные кроссовки",
-                    Price = 4999,
-                    ImageUrl = "https://via.placeholder.com/300x200"
+                    Name = "Air Jordan 1",
+                    Description = "Баскетбольная обувь",
+                    Price = 12999,
+                    ImageUrl = "/images/shoes_jordan.png"
+                },
+
+                new Product
+                {
+                    Name = "Шорты спортивные",
+                    Description = "Спортивные шорты для тренировок",
+                    Price = 1590,
+                    ImageUrl = "/images/shorts1.png"
+                },
+
+                new Product
+                {
+                    Name = "Куртка зимняя",
+                    Description = "Утепленная зимняя куртка с капюшоном",
+                    Price = 7990,
+                    ImageUrl = "/images/winter_jacket.jpg"
+                },
+
+                 new Product
+                {
+                    Name = "Футболка Marcelo Miracles",
+                    Description = "Дизайнерская футболка",
+                    Price = 1299,
+                    ImageUrl = "/images/t-shirt_MM.jpeg"
                 }
             };
 
